f82935e53e3cef4e2ec195e39a8f2fb36f387eb1
saloni196 <45757385+saloni196@users.noreply.github.com>
2018-12-10 18:18:03 +0530

    Add files via upload

---
+++ addEmployee/src/container/Footer.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+
+
+class Footer extends Component {
+/* Renders a Footer  */
+
+    render()
+    {
+
+        return(
+
+            <footer>
+                <h4>Product Horizon</h4>
+            </footer>
+
+        )
+    }
+
+}
+export default Footer;
---
+++ addEmployee/src/container/Nav.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+
+
+class Nav extends Component {
+/* Renders a Navigation  */
+
+    render()
+    {
+
+        return(
+           
+            <nav>
+                <ul>
+                  <li><a href="#">Employee</a></li>              
+                </ul>
+            </nav>
+            
+
+        )
+    }
+
+}
+export default Nav;
